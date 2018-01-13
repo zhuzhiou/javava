0999f1c60106323db0ee4bfcb62a98720c7668a9
zhuzhiou <zhuzhiou@qq.com>
2018-01-13 23:14:19 +0800

    提交代码

--- zego-api/src/main/java/com/jeorgio/javava/thirdparty/zego/service/OpenLiveHandler.java
+++
@@ -1,9 +0,0 @@
-package com.jeorgio.javava.thirdparty.zego.service;
-
-import com.jeorgio.javava.thirdparty.zego.vo.OpenLiveVo;
-
-public interface OpenLiveHandler {
-
-    void handle(OpenLiveVo vo);
-
-}
