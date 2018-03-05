c46bd0cbd3c614b3540c16660b1b48ad357116be
zhuzhiou <zhuzhiou@qq.com>
2018-03-05 13:35:39 +0800

    提交代码

---
+++ javava-shelf-services/src/main/java/cn/javava/shelf/ShelfServicesMain.java
@@ -0,0 +1,23 @@
+package cn.javava.shelf;
+
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ShelfServicesMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfServicesMain.class, args);
+	}
+
+	@Bean
+	public TimeBasedGenerator timeBasedGenerator() {
+		return Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+	}
+}
